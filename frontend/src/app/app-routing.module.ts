eaba5925783f581bed3c0d051a06f3d9aab7785e
Ravali <ravali.Bathula@rohde-schwarz.com>
2019-06-25 23:28:59 +0200

    updated Blog component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { HomepageComponent } from "./homepage/homepage.component";
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   { path: "", component: HomepageComponent },
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  { path: "blog", component: BlogComponent}
 ];
 
 @NgModule({
