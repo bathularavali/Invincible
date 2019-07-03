ff1c9c0959cce07455689f9dca7d5064cf1c1fa2
Pavan Dudi <pavansai.dudi@rohde-schwarz.com>
2019-07-03 15:38:35 +0200

    validation update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  loginUserData = {};
-  constructor(private router: Router, private auth: AuthService) {}
+  loginUserData: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = "";
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private auth: AuthService
+  ) {}
 
-  loginUser() {
+  ngOnInit() {
+    this.loginUserData = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required]
+    });
+  }
+  get f() {
+    return this.loginUserData.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginUserData.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.auth.loginUser(this.loginUserData).subscribe(res => {
+      localStorage.setItem("token", res.token);
+      this.router.navigate(["/"]);
+    });
+  }
+  /* loginUser() {
     this.auth.loginUser(this.loginUserData).subscribe(res => {
       localStorage.setItem('token', res.token);
       this.router.navigate(['/']);
     });
-  }
+  }*/
 }
